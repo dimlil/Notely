944de86de4859aa72c9bda427f139e427db1ad0f
DimVasDev <d.v.vassilev@gmail.com>
2024-04-25 21:41:00 +0300

    feat/ init router

--- Notely-Back-End/config/express.js
+++ Notely-Back-End/config/express.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import { indexRoute } from '../routes/index.js';
 
 export default function setupExpress(app) {
     //Setup the body parser and express.json
     app.use(express.urlencoded({ extended: true}));
     app.use(express.json());
+
+    //Setup the routes
+    app.use('/', indexRoute);
 };
