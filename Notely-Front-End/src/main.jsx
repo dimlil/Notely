77b4d9b5351789b9dbab845b9904e08f6c09b197
DimVasDev <d.v.vassilev@gmail.com>
2024-04-27 00:08:40 +0300

    fix/ import of styles

--- Notely-Front-End/src/main.jsx
+++ Notely-Front-End/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import Navigation from './Navigation.jsx'
-import style from './main.module.scss'
+import style from './assets/main.module.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
