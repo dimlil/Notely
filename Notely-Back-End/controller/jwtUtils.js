9f8db0003fe52694be8f937e0b5c13101985f383
DimVasDev <d.v.vassilev@gmail.com>
2024-04-28 15:03:09 +0300

    feat/ added jwt

---
+++ Notely-Back-End/controller/jwtUtils.js
@@ -0,0 +1,17 @@
+import jwt from 'jsonwebtoken';
+
+export const generateToken = (data) => {
+    const token = jwt.sign(data, process.env.PRIVATE_KEY);
+    return token;
+}
+export const verifyUser = async (req, res, next) => {
+    const { token } = req.session;
+    
+    try {
+        jwt.verify(token, process.env.PRIVATE_KEY);
+        req.isLoggedIn = true;
+    } catch (error) {
+        req.isLoggedIn = false;
+    }
+    next();
+}
