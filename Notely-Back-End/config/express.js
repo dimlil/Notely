ffc9854cc27f4f7dfdcc82bd3086d4b2b0020f12
DimVasDev <d.v.vassilev@gmail.com>
2024-04-28 14:42:03 +0300

    feat/ added cors and new route

--- Notely-Back-End/config/express.js
+++ Notely-Back-End/config/express.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import { indexRoute } from '../routes/index.js';
+import cors from 'cors';
+import { authRoute } from '../routes/auth.js';
 
 export default function setupExpress(app) {
     //Setup the body parser and express.json
-    app.use(express.urlencoded({ extended: true}));
+    app.use(express.urlencoded({ extended: true }));
     app.use(express.json());
+
+    //Setup the cors
+    app.use(cors({ credentials: true, origin: `${process.env.CORS_URL}` }));
 
     //Setup the routes
     app.use('/', indexRoute);
+    app.use('/', authRoute);
 };
