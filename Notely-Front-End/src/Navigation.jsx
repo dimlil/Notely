bfddc620184634126bd71971e82ee6282d4e0ad4
DimVasDev <d.v.vassilev@gmail.com>
2024-04-29 00:36:24 +0300

    feat/ added route guards

--- Notely-Front-End/src/Navigation.jsx
+++ Notely-Front-End/src/Navigation.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import HomePage from './pages/home/HomePage.jsx';
 import Header from './components/header/Header';
 import RegisterPage from './pages/register/RegisterPage.jsx';
@@ -6,18 +6,27 @@ import LoginPage from './pages/login/LoginPage.jsx';
 import CreatePage from './pages/create/CreatePage.jsx';
 import DetailsPage from './pages/details/DetailsPage.jsx';
 import EditPage from './pages/edit/EditPage.jsx';
+import checkAuth from './services/checkAuth.js';
+import { useEffect, useState } from 'react';
 
 function Navigation() {
+    let location = useLocation();
+    const [auth, setAuth] = useState(false);
+    useEffect(() => {
+        checkAuth().then((result) => {
+            setAuth(result)
+        })
+    }, [location])
     return (
         <>
             <Header />
             <Routes >
                 <Route path="/" element={<HomePage />} />
-                <Route path="/register" element={<RegisterPage />} />
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/create" element={<CreatePage />} />
-                <Route path="/note/:id" element={<DetailsPage />} />
-                <Route path="/edit/:id" element={<EditPage />} />
+                <Route path="/register" element={!auth ? <RegisterPage /> : <HomePage />} />
+                <Route path="/login" element={!auth ? <LoginPage /> : <HomePage />} />
+                <Route path="/create" element={auth ? <CreatePage /> : <LoginPage />} />
+                <Route path="/note/:id" element={auth ? <DetailsPage /> : <LoginPage />} />
+                <Route path="/edit/:id" element={auth ? <EditPage /> : <LoginPage />} />
             </Routes>
         </>
     )
