f944a42f3cba165c8ad5c1999970c46fe07d402d
DimVasDev <d.v.vassilev@gmail.com>
2024-04-28 15:05:29 +0300

    feat/ add register functionality

---
+++ Notely-Front-End/src/services/Register.js
@@ -0,0 +1,28 @@
+import axios from "axios"
+export const register = async (name, email, password, rePassword) => {
+    if (name === '' || email === '' || password === '' || rePassword === '') {
+        return {data: "All fields must be filled"}
+    }
+    if (email.length <= 5) {
+        return {data: "The username should be at least 5 characters long"}
+    }
+    if (rePassword !== password) {
+        return {data: "Thw Password and the Confirm Password doesn't match"}
+    }
+    if (password.length <= 4) {
+        return {data: "The password should be at least 4 characters long"}
+    }
+    try {
+        const response = await axios.post(`${import.meta.env.VITE_API_URL}/register`, {
+            name,
+            email,
+            password
+        }, { withCredentials: true });
+        console.log(response);
+        return 'User Is Successfuly Register'
+    } catch (error) {
+        if (error) {
+            return error.massege
+        }
+    }
+}
